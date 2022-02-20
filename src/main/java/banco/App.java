e78fea1228bdadea3225bbb175b14d58f88aa229
alinetenorio <aline.tenorio96@gmail.com>
2022-02-20 00:47:12 -0300

    ajustes em App

--- src/main/java/banco/App.java
+++ src/main/java/banco/App.java
@@ -1,23 +1,7 @@
 package banco;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-
-import banco.domain.banco.Agencia;
-import banco.domain.banco.Banco;
-import banco.domain.conta.Conta;
-import banco.domain.conta.ContaCorrente;
-import banco.domain.pessoa.Funcionario;
-import banco.domain.pessoa.Pessoa;
-import banco.util.Endereco;
-import banco.util.enums.CargoEnum;
-import banco.view.console.BancoView;
 import banco.view.console.InicioView;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
